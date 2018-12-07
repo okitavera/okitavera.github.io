ada83bf9b9c5fdf6bdcd31b4c0ef02b3b56a253a
Nanda Okitavera <codeharuka.yusa@gmail.com>
2018-12-07 10:15:07 +0700

    avoid fixed navbar for now

--- modules/components/okitavera.js
+++ modules/components/okitavera.js
@@ -4,28 +4,17 @@
 
 var overlayButton = document.querySelector("#ovm--btn");
 var overlayMenu = document.querySelector(".overlay-menu");
-overlayMenu.style.display = "none";
+overlayMenu.style.visibility = "hidden";
+overlayMenu.style.opacity = "0";
 overlayButton.onclick = function(){
-  if (overlayMenu.style.display === "none") {
-    overlayMenu.style.display = "block";
+  if (overlayMenu.style.visibility === "hidden") {
+    overlayMenu.style.visibility = "visible";
+    overlayMenu.style.opacity = "1";
     overlayButton.classList.add("opened");
   } else {
-    overlayMenu.style.display = "none";
+    overlayMenu.style.opacity = "0";
+    overlayMenu.style.visibility = "hidden";
     overlayButton.classList.remove("opened");
-  }
-};
-
-var navs = document.querySelector(".nav");
-var alternav = document.querySelector('#alternavTrigger');
-window.onscroll = function() {
-  "use strict";
-  var point = alternav.offsetTop;
-  if (document.body.scrollTop > point || document.documentElement.scrollTop > point) {
-    navs.classList.remove("clears");
-    navs.classList.add("alter");
-  } else {
-    navs.classList.add("clears");
-    navs.classList.remove("alter");
   }
 };
 
